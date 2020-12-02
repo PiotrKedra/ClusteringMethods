352119880af02926939101915f5c71360cac81b7
wiktor <wkilijan@gmail.com>
2020-12-02 18:40:36 +0100

    more datasets

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Hello guys')
+import matplotlib.pyplot as plt
+import pandas as pan
+
+data = pan.read_csv('../../resources/2d_dataset/r15.png')
+
+
+x = data[data.columns[0]]
+y = data[data.columns[1]]
+print(x)
+
+x_np = x.to_numpy()
+
+plt.scatter(x, y)
+plt.show()
