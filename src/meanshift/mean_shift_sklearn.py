34edb2a2ff1a67f9f027a036d13d712cef6e5571
piotrkedra <piotr.k9750@gmail.com>
2020-12-06 00:08:46 +0100

    deleted duplicated txt datasets

--- src/meanshift/mean_shift_sklearn.py
+++ src/meanshift/mean_shift_sklearn.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import MeanShift
 
 # LOAD DATA
-data_file = pd.read_csv('../../resources/2d_dataset/Aggregation.txt', sep="\t", header=None)
+data_file = pd.read_csv('../../resources/2d_dataset/aggregation.csv')
 data_file.columns = ["c1", "c2", "group"]
 data = data_file.iloc[:, 0:2].copy()
 # data_file.plot(kind='scatter', x='c1', y='c2')
