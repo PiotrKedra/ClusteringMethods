28ab636b7f1c207febc00ad72953437a0ed5cf6e
piotrkedra <piotr.k9750@gmail.com>
2020-12-31 15:30:18 +0100

    refactor

--- src/ward/ward.py
+++ src/ward/ward.py
@@ -5,55 +5,6 @@ from math import dist
 import numpy as np
 
 
-def e_value_single_clusters(single_sums_of_squared_deviations, cluster_to_merge_1, cluster_to_merge_2):
-    total_value = single_sums_of_squared_deviations['total_sum']
-    exit_flag = 0
-    for single_sum_object in single_sums_of_squared_deviations['list_of_sums_of_squared_deviations']:
-        if single_sum_object['cluster_number'] == cluster_to_merge_1 \
-                or single_sum_object['cluster_number'] == cluster_to_merge_2:
-            total_value -= single_sum_object['value']
-            if exit_flag == 1:
-                break
-            exit_flag += 1
-    return total_value
-
-
-def update_single_e_values(single_e_values, cluster_to_merge_1, cluster_to_merge_2, clusters):
-    total_sum = single_e_values['total_sum']
-    single_e_values_list = single_e_values['list_of_sums_of_squared_deviations']
-    for i in range(len(single_e_values_list)):
-        if single_e_values_list[i]['cluster_number'] == cluster_to_merge_1:
-            cluster_data_points = clusters[i]['data_points']
-            centroid = get_centroid_of_cluster(cluster_data_points)
-            single_e_value = 0
-            for data_point in cluster_data_points:
-                single_e_value += dist(centroid, data_point)
-            total_sum = total_sum - single_e_values_list[i]['value'] + single_e_value
-            single_e_values_list[i]['value'] = single_e_value
-        elif single_e_values_list[i]['cluster_number'] == cluster_to_merge_2:
-            popped = single_e_values_list.pop(i)
-            total_sum -= popped['value']
-            break
-    single_e_values['total_sum'] = total_sum
-
-
-def update_e_value_matrix(e_value_matrix, single_e_values, clusters, cluster_to_merge_1, cluster_to_merge_2):
-    index_to_update = 0
-    for i in range(len(e_value_matrix)):
-        if e_value_matrix[0][i]['cluster_2_number'] == cluster_to_merge_1:
-            index_to_update = i
-        if e_value_matrix[0][i]['cluster_2_number'] == cluster_to_merge_2:
-            e_value_matrix = np.delete(e_value_matrix, i, 0)
-            e_value_matrix = np.delete(e_value_matrix, i, 1)
-            break
-    for i in range(len(e_value_matrix)-1):
-        for j in range(i+1, len(e_value_matrix)):
-            if i == index_to_update or j == index_to_update:
-                e_value = get_e_value(clusters[i]['data_points'], clusters[j]['data_points'])
-                e_value_matrix[i][j]['e_value'] = e_value
-    return e_value_matrix
-
-
 def ward(df, n_clusters=5):
     clusters = []
     for index, row in df.iterrows():
@@ -61,10 +12,7 @@ def ward(df, n_clusters=5):
 
     single_e_values = init_list_e_value_single(clusters)
     e_value_matrix = init_matrix_for_sum_of_squared_deviations_for_merged_cluster(clusters)
-    """remove column and row
-    matrix = np.delete(e_value_pair_matrix, n, 0)
-    matrix = np.delete(e_value_pair_matrix, n, 1)
-    """
+
     while n_clusters != len(clusters):
         print(len(clusters))
         cluster_to_merge_1 = 0
@@ -82,15 +30,9 @@ def ward(df, n_clusters=5):
                     cluster_to_merge_1 = e_value_matrix[i][j]['cluster_1_number']
                     cluster_to_merge_2 = e_value_matrix[i][j]['cluster_2_number']
 
-        # print(min_e_value)
-        # print(cluster_to_merge_1)
-        # print(cluster_to_merge_2)
         merge_clusters(cluster_to_merge_1, cluster_to_merge_2, clusters)
         update_single_e_values(single_e_values, cluster_to_merge_1, cluster_to_merge_2, clusters)
-        e_value_matrix = update_e_value_matrix(e_value_matrix, single_e_values, clusters, cluster_to_merge_1, cluster_to_merge_2)
-
-    for c in clusters:
-        print(c)
+        e_value_matrix = update_e_value_matrix(e_value_matrix, clusters, cluster_to_merge_1, cluster_to_merge_2)
 
     y = []
     for index, row in df.iterrows():
@@ -101,32 +43,18 @@ def ward(df, n_clusters=5):
     return y
 
 
-def merge_clusters(cluster_to_merge_1, cluster_to_merge_2, clusters):
-    extend_index = 0
-    pop_index = 1
-    for i, cluster in enumerate(clusters):
-        if cluster['cluster_number'] == cluster_to_merge_1:
-            extend_index = i
-        elif cluster['cluster_number'] == cluster_to_merge_2:
-            pop_index = i
-            break
-    cluster_to_merge = clusters.pop(pop_index)
-    clusters[extend_index]['data_points'].extend(cluster_to_merge['data_points'])
-
-
-def get_e_value(cluster1, cluster2):
-    merged_cluster = cluster1 + cluster2
-    centroid = get_centroid_of_cluster(merged_cluster)
-
-    sum_of_squared_deviations = 0
-    for datapoint in merged_cluster:
-        sum_of_squared_deviations += dist(datapoint, centroid)
-
-    return sum_of_squared_deviations
-
-
-def get_centroid_of_cluster(merged_cluster):
-    return mean(merged_cluster, axis=0)
+def init_list_e_value_single(clusters):
+    single_sums_of_squared_deviations = []
+    for cluster in clusters:
+        single_sum_object = {
+            'value': 0,
+            'cluster_number': cluster['cluster_number']
+        }
+        single_sums_of_squared_deviations.append(single_sum_object)
+    return {
+        'total_sum': 0,
+        'list_of_sums_of_squared_deviations': single_sums_of_squared_deviations
+    }
 
 
 def init_matrix_for_sum_of_squared_deviations_for_merged_cluster(clusters):
@@ -148,25 +76,86 @@ def init_matrix_for_sum_of_squared_deviations_for_merged_cluster(clusters):
     return matrix
 
 
-def init_list_e_value_single(clusters):
-    single_sums_of_squared_deviations = []
-    for cluster in clusters:
-        single_sum_object = {
-            'value': 0,
-            'cluster_number': cluster['cluster_number']
-        }
-        single_sums_of_squared_deviations.append(single_sum_object)
-    return {
-        'total_sum': 0,
-        'list_of_sums_of_squared_deviations': single_sums_of_squared_deviations
-    }
+def e_value_single_clusters(single_sums_of_squared_deviations, cluster_to_merge_1, cluster_to_merge_2):
+    total_value = single_sums_of_squared_deviations['total_sum']
+    exit_flag = 0
+    for single_sum_object in single_sums_of_squared_deviations['list_of_sums_of_squared_deviations']:
+        if single_sum_object['cluster_number'] == cluster_to_merge_1 \
+                or single_sum_object['cluster_number'] == cluster_to_merge_2:
+            total_value -= single_sum_object['value']
+            if exit_flag == 1:
+                break
+            exit_flag += 1
+    return total_value
+
+
+def merge_clusters(cluster_to_merge_1, cluster_to_merge_2, clusters):
+    extend_index = 0
+    pop_index = 1
+    for i, cluster in enumerate(clusters):
+        if cluster['cluster_number'] == cluster_to_merge_1:
+            extend_index = i
+        elif cluster['cluster_number'] == cluster_to_merge_2:
+            pop_index = i
+            break
+    cluster_to_merge = clusters.pop(pop_index)
+    clusters[extend_index]['data_points'].extend(cluster_to_merge['data_points'])
+
+
+def update_single_e_values(single_e_values, cluster_to_merge_1, cluster_to_merge_2, clusters):
+    total_sum = single_e_values['total_sum']
+    single_e_values_list = single_e_values['list_of_sums_of_squared_deviations']
+    for i in range(len(single_e_values_list)):
+        if single_e_values_list[i]['cluster_number'] == cluster_to_merge_1:
+            cluster_data_points = clusters[i]['data_points']
+            centroid = get_centroid_of_cluster(cluster_data_points)
+            single_e_value = 0
+            for data_point in cluster_data_points:
+                single_e_value += dist(centroid, data_point)
+            total_sum = total_sum - single_e_values_list[i]['value'] + single_e_value
+            single_e_values_list[i]['value'] = single_e_value
+        elif single_e_values_list[i]['cluster_number'] == cluster_to_merge_2:
+            popped = single_e_values_list.pop(i)
+            total_sum -= popped['value']
+            break
+    single_e_values['total_sum'] = total_sum
+
+
+def update_e_value_matrix(e_value_matrix, clusters, cluster_to_merge_1, cluster_to_merge_2):
+    index_to_update = 0
+    for i in range(len(e_value_matrix)):
+        if e_value_matrix[0][i]['cluster_2_number'] == cluster_to_merge_1:
+            index_to_update = i
+        if e_value_matrix[0][i]['cluster_2_number'] == cluster_to_merge_2:
+            e_value_matrix = np.delete(e_value_matrix, i, 0)
+            e_value_matrix = np.delete(e_value_matrix, i, 1)
+            break
+    for i in range(len(e_value_matrix)-1):
+        for j in range(i+1, len(e_value_matrix)):
+            if i == index_to_update or j == index_to_update:
+                e_value = get_e_value(clusters[i]['data_points'], clusters[j]['data_points'])
+                e_value_matrix[i][j]['e_value'] = e_value
+    return e_value_matrix
+
+
+def get_e_value(cluster1, cluster2):
+    merged_cluster = cluster1 + cluster2
+    centroid = get_centroid_of_cluster(merged_cluster)
+
+    sum_of_squared_deviations = 0
+    for datapoint in merged_cluster:
+        sum_of_squared_deviations += dist(datapoint, centroid)
+
+    return sum_of_squared_deviations
+
+
+def get_centroid_of_cluster(merged_cluster):
+    return mean(merged_cluster, axis=0)
 
 
 data = pd.read_csv('../../resources/2d_dataset/jain.csv')
 data.drop(['label'], axis=1, inplace=True)
 # data = pd.read_csv('../meanshift/testing.csv')
-
-clusters = ward(data, n_clusters=2)
-
-plt.scatter(data[['x']], data[['y']], c=clusters)
+y_c = ward(data, n_clusters=2)
+plt.scatter(data[['x']], data[['y']], c=y_c)
 plt.show()
