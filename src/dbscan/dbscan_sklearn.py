9ae757d0bc762572a5148c921dde40a7f0c6502f
wiktor <wkilijan@gmail.com>
2021-01-12 23:01:01 +0100

    fixed - returns values

--- src/dbscan/dbscan_sklearn.py
+++ src/dbscan/dbscan_sklearn.py
@@ -1,18 +1,32 @@
+def warn(*args, **kwargs):
+    pass
+
+
+import warnings
+
+warnings.warn = warn
 import pandas as pd
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
+import numpy as np
 
-# LOAD DATA
-df = pd.read_csv('../../resources/2d_dataset/compound.csv')
-data = df.to_numpy()
 
-# PREPARE MODEL
+def dbscan(df, my_eps, my_min_samples):
+    # LOAD DATA
+    # df = pd.read_csv('../../resources/2d_dataset/compound.csv')
+    data = df.to_numpy()
 
-dbscan = DBSCAN(eps=1, min_samples=3)
-dbscan.fit(data)
-y_dbscan = dbscan.fit_predict(data)
+    # PREPARE MODEL
 
-# SHOW CLUSTERS
-plt.scatter(data[:, 0], data[:, 1], c=y_dbscan)
-pyplot.show()
+    dbscan = DBSCAN(eps=my_eps, min_samples=my_min_samples)
+    dbscan.fit(data)
+    y_dbscan = dbscan.fit_predict(data)
+
+    # SHOW CLUSTERS
+    # plt.scatter(data[:, 0], data[:, 1], c=y_dbscan)
+    # pyplot.show()
+
+    data = np.concatenate((data[:, :-1], y_dbscan.reshape((y_dbscan.shape[0], 1))), axis=1)
+
+    return data
--- src/k_means/k_means_sklearn.py
+++ src/k_means/k_means_sklearn.py
@@ -2,19 +2,27 @@ import pandas as pd
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import numpy as np
 
-# LOAD DATA
-df = pd.read_csv('../../resources/2d_dataset/r15.csv')
-data = df.to_numpy()
 
-print(data)
+def k_means(df, clusters):
+    # LOAD DATA
+    # df = pd.read_csv('../../resources/2d_dataset/r15.csv')
+    data = df.to_numpy()
 
-# PREPARE MODEL
+    # print(data)
 
-kmeans = KMeans(n_clusters=15)
-kmeans.fit(data)
-y_kmeans = kmeans.predict(data)
+    # PREPARE MODEL
 
-# SHOW CLUSTERS
-plt.scatter(data[:, 0], data[:, 1], c=y_kmeans)
-pyplot.show()
+    kmeans = KMeans(n_clusters=clusters)
+    kmeans.fit(data)
+    y_kmeans = kmeans.predict(data)
+
+    data = np.concatenate((data[:, :-1], y_kmeans.reshape((y_kmeans.shape[0], 1))), axis=1)
+
+    # print(data)
+    # SHOW CLUSTERS
+    plt.scatter(data[:, 0], data[:, 1], c=y_kmeans)
+    pyplot.show()
+
+    return data
